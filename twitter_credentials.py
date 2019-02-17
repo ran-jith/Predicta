adf214842bf3fa6e79abc2532220818b28383d50
Shijith m <35588484+shijithm7@users.noreply.github.com>
2019-02-17 19:57:52 +0530

    Add files via upload

---
+++ twitter_credentials.py
@@ -0,0 +1,9 @@
+# Variables that contains the user credentials to access Twitter API 
+
+
+# uses real accessing keys instead of 'xxxxx' we can't provide these authenticating twitter keys for 2nd party.
+
+ACCESS_TOKEN = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
+ACCESS_TOKEN_SECRET = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
+CONSUMER_KEY = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
+CONSUMER_SECRET = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
